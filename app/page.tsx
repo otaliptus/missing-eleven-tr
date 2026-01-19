48d2ecb3e3d632958434553c1d51361e4bc61c52
otaliptus <omertalib42@gmail.com>
2026-01-19 12:20:08 +0100

    fix: eslint

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export const dynamic = "force-dynamic"
 // retarded ran-gen
 function mulberry32(a: number) {
   return function() {
-    var t = a += 0x6D2B79F5;
+    let t = a += 0x6D2B79F5;
     t = Math.imul(t ^ t >>> 15, t | 1);
     t ^= t + Math.imul(t ^ t >>> 7, t | 61);
     return ((t ^ t >>> 14) >>> 0) / 4294967296;
